b7562338e51bb92191bb09e78efc1f167403359a
Mathis Meric <mathis.meric-pons@etudiant.univ-brest.fr>
2023-01-24 09:00:06 +0000

    refactor(colors): change name of functionnalGrey

--- config/colors.js
+++ config/colors.js
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-const COLORS = {
-  white: "#EBE5E2",
-  black: "#272532",
-  primary: "#373B69",
-  secondary: "#13688C",
-  tertiary: "#83CFDF",
-  functionnalGrey: "#708ED2",
-  red: "#960018",
-  green: "#50C878",
-  orange: "#F96815",
-};
-module.exports = COLORS;
-=======
+
 export const COLORS = {
   white: "#EBE5E2",
   black: "#272532",
   primary: "#373B69",
   secondary: "#13688C",
   tertiary: "#83CFDF",
-  functionnal_grey: "#708ED2",
+  functionnalGrey: "#708ED2",
 };
->>>>>>> 62722d1 (feat: add colors configuration)
+
+

779ea5dd12ff6ff9c3c90431cdb52f27bd41d21e
Garcia-Christophe <garciachristophe29@gmail.com>
2023-01-26 13:53:27 +0100

    feat(API): add actor filter in search section
    Can search an actor and a movie by an actor present in its cast

--- src/app.js
+++ src/app.js
@@ -19,5 +19,34 @@ async function getMovieByName(name) {
   console.log(data.results);
 }
 
+async function getActor(name) {
+  const response = await fetch(
+    `${API_URL}search/person?${API_KEY_AND_CRITERIA}&query=${name}`
+  );
+  const data = await response.json();
+  console.log(data.results);
+}
+
+async function getMovieByActor(name) {
+  const response = await fetch(
+    `${API_URL}search/person?${API_KEY_AND_CRITERIA}&query=${name}`
+  );
+  var movies = [];
+  response.json().then((data) => {
+    data.results.forEach((actor) => {
+      fetch(
+        `${API_URL}discover/movie?${API_KEY_AND_CRITERIA}&with_cast=${actor.id}`
+      ).then((response) => {
+        response.json().then((data) => {
+          movies.push(data.results);
+        });
+      });
+    });
+  });
+  console.log(movies);
+}
+
 getMovies();
 getMovieByName("Harry Potter");
+getActor("Chris Hemsworth");
+getMovieByActor("Chris Hemsworth");
